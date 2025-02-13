f24050327f68e89246274d79c7198fc09d73b004
Annanya garg <gargannanya00@gmail.com>
2025-02-13 17:31:07 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -3,10 +3,10 @@ int main(){
     int a,b;
     scanf("%d%d",&a,&b);
     if(a%b==0){
-    printf("Divisible");
+    printf("Yes");
     }
     else{
-        printf("Not Divisible");
+        printf("No");
     }
     return 0;
 }
