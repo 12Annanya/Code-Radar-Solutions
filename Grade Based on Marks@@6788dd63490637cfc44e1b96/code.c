d3fcd786441da149ced8046a7feef04138f4b877
Annanya garg <gargannanya00@gmail.com>
2025-02-13 16:40:13 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,f;
+    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&f);
+    if (a>=90){
+        printf("A");
+    }
+    else if(80<=a<90){
+        printf("B");
+    }
+    else if(a70<=a<80){
+        printf("C");
+    }
+    else if(60<=a<70){
+        printf("D");
+    }
+    else{
+        printf("F")
+    }
+}
